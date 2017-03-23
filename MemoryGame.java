cf38464b98cef6b53b9f94f6b821d1b42fba04de
taylorealand <taylorealand144@pointloma.edu>
2017-03-23 16:16:55 -0700

    Add files via upload

---
+++ MemoryGame.java
@@ -0,0 +1,20 @@
+//Memory game project
+//Taylor E, Ashleigh M, Phil V.
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class MemoryGame extends JFrame
+{
+   private Container contents;
+   private JLabel Level, Score, Timer;
+   private JButton Start;
+   private JPanel scorePanel, gamePanel, scorePanel, timerstartPanel;
+   public MemoryGame()
+   {
+      super("Memory Game");
+      contents=getContentPane();
+      contents.setLayout(new BorderLayout());
+      
+   }
+
+}
